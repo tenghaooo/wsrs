59ad9e6513ecd6a25cefb24c14b69396ad9c003b
tenghaooo <tenghao.huang@acercsi.com>
2020-10-20 21:56:37 +0800

    project start

---
+++ CaseInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wsrs
+{
+    class CaseInfo
+    {
+        public string userName { get; set; }
+        public string projectName { get; set; }
+        public string reportName { get; set; }
+        public string period { get; set; }
+        public string reportCode { get; set; }
+        public string author { get; set; }
+        public string tool { get; set; }
+        public string year { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string level { get; set; }
+        public CaseInfo() { }
+
+        public CaseInfo(string userName, string projectName, string reportName, string period, string reportCode, string author, string tool, string year, string startDate, string endDate, string level)
+        {
+            this.userName = userName;
+            this.projectName = projectName;
+            this.reportName = reportName;
+            this.period = period;
+            this.reportCode = reportCode;
+            this.author = author;
+            this.tool = tool;
+            this.year = year;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.level = level;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using System;
+
+namespace wsrs
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+
+            
+            var excelApp = new Excel.Application();
+            excelApp.Visible = false;
+
+            // 開啟驗證結果Excel表
+            Excel.Workbook excelBook = excelApp.Workbooks.Open("D:\\vsprojects\\wsrs\\TemplateFiles\\sample.xlsx");
+
+            // 設定CaseInfo
+            Excel.Worksheet caseInfoSheet = excelBook.Worksheets["caseinfo"];
+            CaseInfo caseinfo = new CaseInfo();
+            caseinfo = getCaseInfo(caseInfoSheet);
+
+            excelBook.Close();
+            excelApp.Quit();
+
+            Console.WriteLine(caseinfo.author);
+            Console.ReadLine();
+        }
+
+        static CaseInfo getCaseInfo(Excel.Worksheet sheet)
+        {
+            var caseinfo = new CaseInfo();
+            caseinfo.userName = sheet.Cells[2, "A"].Value.ToString();
+            caseinfo.projectName = sheet.Cells[2, "B"].Value.ToString();
+            caseinfo.reportName = sheet.Cells[2, "C"].Value.ToString();
+            caseinfo.period = sheet.Cells[2, "D"].Value.ToString();
+            caseinfo.reportCode = sheet.Cells[2, "E"].Value.ToString();
+            caseinfo.author = sheet.Cells[2, "F"].Value.ToString();
+            caseinfo.tool = sheet.Cells[2, "G"].Value.ToString();
+            caseinfo.year = sheet.Cells[2, "H"].Value.ToString();
+            caseinfo.startDate = sheet.Cells[2, "I"].Value.ToString();
+            caseinfo.endDate = sheet.Cells[2, "J"].Value.ToString();
+            caseinfo.level = sheet.Cells[2, "K"].Value.ToString();
+            return caseinfo;
+        }
+    }
+}
